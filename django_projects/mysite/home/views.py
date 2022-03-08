5584175a30b9b9233245718384ec17e358bd9d02
pure-cheekbones <52796958+pure-cheekbones@users.noreply.github.com>
2022-03-08 19:19:11 +0200

    refactor home app

--- django_projects/mysite/home/views.py
+++ django_projects/mysite/home/views.py
@@ -11,19 +11,21 @@ class AppsIndexUrls(ListView):
     template_name = "home/main.html"
     context_object_name = "app_index_urls"
 
-    def app_is_third_party(self, app: str) -> bool:
-        return not (
-            app.startswith("django.")
-            or app.startswith("home.")
-            or app.startswith("django_extensions")
+    def is_third_party(self, app: str) -> bool:
+        filtered_apps: tuple = ("home.apps.HomeConfig",)
+        result: bool = (
+            (app not in filtered_apps)
+            and (".apps." in app)
+            and (app.endswith("Config"))
         )
+        return result
 
     def get_queryset(self):
         """returns a list of user-made and third-party app index urls"""
         app_index_urls = []
         for app in settings.INSTALLED_APPS:
-            if self.app_is_third_party(app):
-                app_index_url = app.split(".")  # ?app.etc.etc
+            if self.is_third_party(app):
+                app_index_url = app.split(".")  # ?app.apps.AppConfig
                 app_index_urls.append(app_index_url[0])
 
         return app_index_urls
