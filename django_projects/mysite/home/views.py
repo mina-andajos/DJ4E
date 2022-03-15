5e63852dffe571a5504b0c90fd499593177038ad
pure-cheekbones <52796958+pure-cheekbones@users.noreply.github.com>
2022-03-15 21:04:56 +0200

    refactor home

--- django_projects/mysite/home/views.py
+++ django_projects/mysite/home/views.py
@@ -1,16 +1,15 @@
 import logging
 
 from django.conf import settings
-from django.views.generic import ListView
+from django.views import View
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 # Create your views here.
-class AppsIndexUrls(ListView):
-    template_name = "home/main.html"
-    context_object_name = "app_index_urls"
-
+class AppsIndexUrls(View):
     def is_third_party(self, app: str) -> bool:
         filtered_apps: tuple = ("home.apps.HomeConfig",)
         result: bool = (
@@ -20,8 +19,9 @@ class AppsIndexUrls(ListView):
         )
         return result
 
-    def get_queryset(self):
+    def get_third_party_apps(self):
         """returns a list of user-made and third-party app index urls"""
+
         app_index_urls = []
         for app in settings.INSTALLED_APPS:
             if self.is_third_party(app):
@@ -29,3 +29,21 @@ class AppsIndexUrls(ListView):
                 app_index_urls.append(app_index_url[0])
 
         return app_index_urls
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        template_name = "home/main.html"
+        app_index_urls = self.get_third_party_apps()
+
+        host = request.get_host()
+        is_local: bool = "localhost" in host or "127.0.0.1" in host
+
+        context = {
+            "app_index_urls": app_index_urls,
+            "islocal": is_local,
+        }
+
+        return render(
+            request=request,
+            template_name=template_name,
+            context=context,
+        )
