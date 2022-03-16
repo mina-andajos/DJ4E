7abca4eda100ae732129312b0d5ab0dfd47f5470
pure-cheekbones <52796958+pure-cheekbones@users.noreply.github.com>
2022-03-17 01:21:58 +0200

    add functionality to ads app

--- django_projects/mysite/ads/views.py
+++ django_projects/mysite/ads/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .models import Ad
+from .owner import (
+    OwnerListView,
+    OwnerDetailView,
+    OwnerCreateView,
+    OwnerUpdateView,
+    OwnerDeleteView,
+)
 
 # Create your views here.
+class AdListView(OwnerListView):
+    model = Ad
+    # By convention:
+    # template_name = "myarts/Ad_list.html"
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ["title", "price", "text"]
+
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ["title", "price", "text"]
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
